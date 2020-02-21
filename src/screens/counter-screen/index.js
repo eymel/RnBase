5f276a4df812e1d5e4a711bc72cb517bb1bdf3e8
eymel <eyup.yildirim02@gmail.com>
2020-02-21 15:40:32 +0300

    counter screen updated with mobx

--- src/screens/counter-screen/index.js
+++ src/screens/counter-screen/index.js
@@ -1,41 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import {StyleSheet} from 'react-native';
 import {Container, Content, Button, Text, View} from 'native-base';
 
-export function CounterScreen({navigation}) {
-  const [counter, setCounter] = useState(0);
+import { observer } from 'mobx-react';
+import  CounterStore from '../../stores/counter-store';
 
-  return (
-    <Container>
-      <Content contentContainerStyle={styles.container}>
-        <Text>Counter Screen</Text>
-        <Text  style={styles.counter_text}>{counter}</Text>
-        <View style={styles.counterButtons}>
-          <Button
-            danger
-            style={styles.button}
-            onPress={() => {
-              setCounter(counter - 1);
-            }}>
-            <Text style={styles.button_text}> - </Text>
-          </Button>
-          <Button
-            success
-            style={styles.button}
-            onPress={() => {
-              setCounter(counter + 1);
-            }}>
-            <Text style={styles.button_text}> + </Text>
-          </Button>
-        </View>
 
-        <Button onPress={() => navigation.navigate('Details')}>
-          <Text>Go to Details Screen</Text>
-        </Button>
-      </Content>
-    </Container>
-  );
-}
+@observer
+export class CounterScreen extends Component { 
+  render() {
+             return (
+               <Container>
+                 <Content contentContainerStyle={styles.container}>
+                   <Text>Counter Screen</Text>
+                   <Text style={styles.counter_text}>{CounterStore.count}</Text>
+                   <View style={styles.counterButtons}>
+                     <Button
+                       danger
+                       style={styles.button}
+                       onPress={() => {
+                         CounterStore.decrement();
+                       }}>
+                       <Text style={styles.button_text}> - </Text>
+                     </Button>
+                     <Button
+                       success
+                       style={styles.button}
+                       onPress={() => {
+                         CounterStore.increment();
+                       }}>
+                       <Text style={styles.button_text}> + </Text>
+                     </Button>
+                   </View>
+
+                   <Button onPress={() => navigation.navigate('Details')}>
+                     <Text>Go to Details Screen</Text>
+                   </Button>
+                 </Content>
+               </Container>
+             );
+           }
+  
+};
+
+
 const styles = StyleSheet.create({
   container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
   counter_text: {fontSize: 50, fontWeight: 'bold'},
