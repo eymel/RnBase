93c2c87dcd0e8e08e1c5b3e23de1c73321841c9d
eymel <eyup.yildirim02@gmail.com>
2020-03-11 16:00:40 +0300

    app.js file moved to src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import { RootStack } from './src/navigations';
-import {  Root } from 'native-base';
+import {RootStack} from './navigations';
+import {Root} from 'native-base';
 
 function App() {
   return (
